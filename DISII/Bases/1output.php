5c0b035118bc9a66c0475d857399d3c70bfb22ef
kevin FERRANDON <kferrandon@gmail.com>
2019-12-18 11:53:05 +0100

    Update 1output.php

--- DISII/Bases/1output.php
+++ DISII/Bases/1output.php
@@ -7,11 +7,12 @@
     <body>
       <h1>Cours PHP</h1>
         <?php
-            echo "<h2>Première instruction je génère du html avec l'instruction Echo de PHP </h2>";
-            echo 'Bonjour à tous !<br>';
+            echo "<h2>PremiÃ¨re instruction je gÃ©nÃ¨re du html avec l'instruction Echo de PHP </h2>";
+            echo 'Bonjour Ã  tous !<br>';
             echo "<br>J'ai ", 37, " ans.<br>";
-            print "<h2>deuxieme instruction je génère du html avec PHP avec print</h2>";
-            print 'Bonjour à tous !<br>';
+            print "<h2>deuxieme instruction je gÃ©nÃ¨re du html avec PHP avec print</h2>";
+            print 'Bonjour Ã  tous !<br>';
         ?>
         <p>contenu static</p>
     </body>
+</html>
